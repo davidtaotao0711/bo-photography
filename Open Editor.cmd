9ed1395acc722e7094cbf5a346523a8e24b88b5d
davidtaotao0711 <leshy00521@163.com>
2026-07-19 17:03:15 +0800

    Revert "Merge portfolio archive redesign"
    This reverts commit a720b3d7a0f53ede9f1d926695f319cbdfb6b7f4, reversing
    changes made to 3e7882399077614afeb9d90a9f6bf7e95f36ff70.

--- Open Editor.cmd
+++ Open Editor.cmd
@@ -1,2 +1,17 @@
 @echo off
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0scripts\open-local-editor.ps1"
+setlocal
+
+cd /d "%~dp0"
+set "PATH=C:\Program Files\nodejs;%PATH%"
+
+echo Starting Bo David local editor...
+echo.
+echo If the browser does not open automatically, visit:
+echo http://localhost:4321/editor
+echo.
+
+npm.cmd run editor
+
+echo.
+echo Editor has stopped. You can close this window.
+pause
